2f567909fab7ee4b39ff4d3116172e73458db05d
Mostafa Elsayed <moelsayed949@gmail.com>
2023-11-13 22:08:50 +0200

    edit json server

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./home.css"
+import axios from 'axios'
 const Home = () => {
+    const [data, setData] = useState([])
+    useEffect(() => {
+        axios.get("http://localhost:3005/mobiles").then((response) => {
+            setData(response.data);
+        });
+    }, []);
     return (
-        <div className='banner-img'>
+        <>
+            <div className='banner-img'>
+            </div>
+            {data.map((product) => (
+                <div key={product.id}>
+                    <h1>{product.category}</h1>
+                    <p>{product.description}</p>
+                </div>
+            ))}
+        </>
 
-        </div>
     )
 }
 
