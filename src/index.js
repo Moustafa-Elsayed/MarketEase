cc17fe2b6d874a5aead89893189a78d85fc0c067
Mostafa Elsayed <moelsayed949@gmail.com>
2023-11-14 01:57:52 +0200

    update data of json server

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
 import { Provider } from 'react-redux'
 import { store } from './redux/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,7 +10,6 @@ root.render(
     <Provider store={store}>
       <App />
     </Provider>
-
   </React.StrictMode>
 );
 
--- src/redux/productSlice.jsx
+++ src/redux/productSlice.jsx
@@ -1,14 +1,44 @@
-import { createSlice } from '@reduxjs/toolkit'
+/* eslint-disable no-empty-pattern */
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import axios from 'axios';
 
-const initialState = {
-  value: 0,
-}
+export const fetchProduct = createAsyncThunk("productSlice/fetchProduct",
+  async (_, thunkAPI) => {
+    const { rejectWithValue } = thunkAPI;
+    try {
+      const response = await axios.get("http://localhost:3005/mobiles");
+      return response.data;
+    } catch (error) {
+      // Handle any errors here
+      return rejectWithValue(error.message);
+    }
+  })
 
 export const productSlice = createSlice({
   name: 'product',
-  initialState,
+  initialState: {
+    data: [],
+    isloading: false,
+    error: false,
+  },
   reducers: {
-   
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchProduct.pending, (state, action) => {
+        state.isloading = true;
+        state.error = false;
+      })
+      .addCase(fetchProduct.fulfilled, (state, action) => {
+        state.data = action.payload;
+        state.isloading = false;
+        state.error = false;
+      })
+
+      .addCase(fetchProduct.rejected, (state, action) => {
+        state.isloading = false;
+        state.error = action.payload;
+      });
   },
 })
 
