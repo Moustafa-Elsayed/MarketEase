00da0a74577b47bb3be9021733ddd67cd1b0eb21
Mostafa Elsayed <moelsayed949@gmail.com>
2023-11-14 21:38:18 +0200

    map product from redux toolkit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,71 +1,16 @@
-import { useDispatch, useSelector } from "react-redux"
+import Product from "../product/Product"
 import "./home.css"
-import { fetchProduct } from "../../redux/productSlice";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
-import {  Grid } from "@mui/material";
-import { useEffect } from "react";
+import Container from '@mui/material/Container'
+
 const Home = () => {
-    const product = useSelector((state) => state.product.data)
-    const dispatch = useDispatch();
-    useEffect(() => {
-        dispatch(fetchProduct())
-    }, [dispatch])
+   
     return (
         <>
             <div className='banner-img'>
             </div>
-
-            <Grid container spacing={5}>
-                {product.map((product) => (
-                    <Grid key={product.id} xs={12} sm={6} md={4} lg={3} item>
-                        <Card
-                            elevation={2}
-                            className="card-hover"
-                            sx={{
-                                maxWidth: "100%",
-                                borderRadius: "9px",
-                                height: "100%",
-                                padding: "10px",
-                            }}
-                            onClick={() => { }}
-                        >
-
-                            <Typography
-                                gutterBottom
-                                variant="h5"
-                                component="div"
-                                sx={{
-                                    paddingTop: "10px",
-                                    color: "#343a40",
-                                    fontWeight: "600",
-                                }}
-                            >
-                                {product.category}
-                            </Typography>
-                            <Typography
-                                variant="body2"
-                                sx={{ marginBottom: "5px", color: "#f26522" }}
-                            >
-                                {product.price}$
-                            </Typography>
-
-                            <CardMedia
-                                component="img"
-                                height="180"
-                                image={product.image}
-                                alt="green iguana"
-                                sx={{ paddingTop: "10px" }}
-                            />
-                            <CardContent>
-
-                            </CardContent>
-                        </Card>
-                    </Grid>
-                ))}
-            </Grid>
+            <Container maxWidth="lg" sx={{paddingTop:"20px"}}>              
+            <Product />
+            </Container>
         </>
 
     )
